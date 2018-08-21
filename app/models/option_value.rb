77159ffcf1d189a2dd59718ae07c077dbd686af4
Sahil Gadimbayli <sahil@betterpayment.de>
2018-08-21 16:37:31 +0200

    Add validation for Name's presence in Option Value model

--- app/models/option_value.rb
+++ app/models/option_value.rb
@@ -3,5 +3,5 @@ class OptionValue < ApplicationRecord
   has_many :single_option_values, inverse_of: :option_value, dependent: :destroy
   has_many :variants, through: :single_option_values, dependent: :destroy
 
-  validates :name, uniqueness: { scope: :option_id, case_sensitive: false }
+  validates :name, presence: true, uniqueness: { scope: :option_id, case_sensitive: false }
 end
